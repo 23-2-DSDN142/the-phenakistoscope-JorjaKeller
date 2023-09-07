ce9255a35085d76c4c0a30ebaaac7c8030ac6578
JorjaKeller <jorja.keller@gmail.com>
2023-09-07 20:30:33 +1200

    commit

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,4 +1,4 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 14;
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
@@ -6,19 +6,36 @@ function setup_pScope(pScope){
   pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+
+  pScope.load_image("Bug","png")
+  pScope.load_image_sequence("Lizard","png",14);
+
+  angleMode(DEGREES);
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 148, 117, 78);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  var layer1 = new PLayer(Lizard);
+  layer1.mode( RING );
+  layer1.set_boundary( 200, 900 );
 
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
+  var layer2 = new PLayer(Bug);
+  layer2.mode( SWIRL(2) );
   layer2.set_boundary( 0, 400 );
+
+  var layer3 = new PLayer(Ant);
+layer3.mode(RING);
+layer3.set_boundary(900,1000);
+
+}
+
+function Lizard(x,y,animation,pScope){
+  push();
+  rotate(180);
+  pScope.draw_image_from_sequence("Lizard",x,y+665,animation.frame);
+  pop();
 }
 
 function faces(x, y, animation, pScope){
@@ -33,17 +50,30 @@ function faces(x, y, animation, pScope){
 
 }
 
-function squares(x, y, animation, pScope){
+//function Ant(x, y, animation, pScope){
 
   // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
+  //let angleOffset = (360 / SLICE_COUNT) / 2
+  // let backgroundArcStart = 270 - angleOffset;
+  // let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+ // fill(255)
+ // rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+//}
 
+function Ant(x, y, animation, pScope){
+  fill(54, 26, 28)
+  ellipse(100,950,20,20)
+  ellipse(115,950,20,20)
+  ellipse(80,950,35,20)
+}
+  function Bug(x,y, animation, pScope){
+   /*let angleOffset = (360 / SLICE_COUNT) / 2
+    let backgroundArcStart = 270 - angleOffset;
+    let backgroundArcEnd = 270 + angleOffset;*/
+    pScope.draw_image("Bug",x,y)
+
+  
 }
